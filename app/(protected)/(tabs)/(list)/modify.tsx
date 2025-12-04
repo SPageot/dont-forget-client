25c51f347d13240f909a9d3e662af38155c38921
Stanley Pageot <stanley.pageot@gmail.com>
2025-12-04 16:01:54 -0500

    add list to modify page

--- app/(protected)/(tabs)/(list)/modify.tsx
+++ app/(protected)/(tabs)/(list)/modify.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from '@/styles/background';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+import { BASE_API } from '@/util/baseApi';
+import { useStore } from '@/store/store';
+import ItemContainer from '@/components/ItemContainer';
+import { ListProps } from '@/types/list';
 
 export default function ModifyList() {
+  const [list, setList] = useState<string[] | []>([]);
+  const user = useStore((state: any) => state.user);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const res = await axios.get(`${BASE_API}/lists/${user._id}`);
+      if (res.data) {
+        setList(res.data.map((item: ListProps) => item.title));
+      }
+    };
+
+    fetchList();
+  }, []);
+
   return (
     <LinearGradient
       colors={['#f5f7a1', '#ffd194', '#ff9a8b']}
@@ -12,7 +31,22 @@ export default function ModifyList() {
       end={{ x: 1, y: 1 }}
       style={styles.background}
     >
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView>
+        <FlatList
+          data={list}
+          renderItem={({ item }) => (
+            <ItemContainer
+              listItem={item}
+              onRemovePress={() =>
+                setList((prev) =>
+                  prev.filter((removeItem) => removeItem != item)
+                )
+              }
+            />
+          )}
+          keyExtractor={(item) => item}
+        />
+      </SafeAreaView>
     </LinearGradient>
   );
 }
--- types/list.ts
+++ types/list.ts
@@ -1,1 +1,7 @@
-
+export type ListProps = {
+  _id: any;
+  createdAt: Date;
+  listItems: string[];
+  title: string;
+  userId: string;
+};
